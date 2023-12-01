51ae8d65e542ae0c6ac87d33b82b4cdcaebe8514
Jairo Martinez <ocrensys@gmail.com>
2023-12-01 14:15:09 -0600

    feat(settings): :art: add angular-material
    adding angular material to use their component and utils elements
    
    • angular-material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSidenavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-marvel-test';
+  title = 'Marvel Test';
 }
