744f7d8f6bb9362327d1e16d40483f7ecd6d5270
Jairo Martinez <ocrensys@gmail.com>
2024-01-04 22:51:24 -0600

    perf(tools): :art: add tools to make clean code
    config libreries such as prettier, eslint and hunsky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,60 @@
-import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, OnDestroy } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnDestroy, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-import { FooterComponent } from './components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { SideMenuComponent } from './components/sidemenu/sidemenu.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    /* ANGULAR MATERIAL COMPONENTS */
-    MatSidenavModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatToolbarModule,
-    MatListModule,
-    MatIconModule,
-    /* CUSTOM COMPONENTS */
-    BreadcrumbComponent,
-    FooterComponent,
-    SideMenuComponent,
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		/* ANGULAR MATERIAL COMPONENTS */
+		MatSidenavModule,
+		MatButtonModule,
+		MatDividerModule,
+		MatToolbarModule,
+		MatListModule,
+		MatIconModule,
+		/* CUSTOM COMPONENTS */
+		BreadcrumbComponent,
+		FooterComponent,
+		SideMenuComponent
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent implements OnDestroy {
-  protected title = 'Ocrensys';
-  protected device = signal<'mobile' | 'desktop'>('mobile');
-  protected opened = signal<boolean>(false);
-  protected mobileQuery: MediaQueryList;
-  private _mobileQueryListener: (event: any) => void;
+	protected title = 'Ocrensys';
+	protected device = signal<'mobile' | 'desktop'>('mobile');
+	protected opened = signal<boolean>(false);
+	protected mobileQuery: MediaQueryList;
+	private _mobileQueryListener: (event: MediaQueryListEvent) => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 768px)');
-    this._mobileQueryListener = (event) => {
-      console.log(event.matches);
-      if (event.matches) {
-        this.device.set('mobile');
-        this.opened.set(false);
-      } else {
-        this.device.set('desktop');
-        this.opened.set(true);
-      }
+	constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+		this.mobileQuery = media.matchMedia('(max-width: 768px)');
+		this._mobileQueryListener = (event: MediaQueryListEvent): void => {
+			console.log(event.matches);
+			if (event.matches) {
+				this.device.set('mobile');
+				this.opened.set(false);
+			} else {
+				this.device.set('desktop');
+				this.opened.set(true);
+			}
+			return changeDetectorRef.detectChanges();
+		};
+		this.mobileQuery.addListener(this._mobileQueryListener);
+	}
 
-      return changeDetectorRef.detectChanges();
-    };
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
+	ngOnDestroy(): void {
+		this.mobileQuery.removeListener(this._mobileQueryListener);
+	}
 }
--- src/app/pages/comics/list/list.component.ts
+++ src/app/pages/comics/list/list.component.ts
@@ -6,23 +6,23 @@ import { Observable } from 'rxjs';
 import { TComic } from '../../../shared/types/comic.type';
 import { GetComicsAction } from '../../../state/actions/comics.action';
 import {
-  ComicsSelector,
-  LoadingSelector,
+	ComicsSelector,
+	LoadingSelector
 } from '../../../state/selectors/comics.selector';
 
 @Component({
-  selector: 'app-comics-list',
-  standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule],
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.css',
+	selector: 'app-comics-list',
+	standalone: true,
+	imports: [CommonModule, MatProgressSpinnerModule],
+	templateUrl: './list.component.html',
+	styleUrl: './list.component.css'
 })
 export class ComicsListComponent implements OnInit {
-  private readonly _store = inject(Store);
-  protected comics$: Observable<TComic[]> = this._store.select(ComicsSelector);
-  protected loading$: Observable<boolean> = this._store.select(LoadingSelector);
+	private readonly _store = inject(Store);
+	protected comics$: Observable<TComic[]> = this._store.select(ComicsSelector);
+	protected loading$: Observable<boolean> = this._store.select(LoadingSelector);
 
-  ngOnInit(): void {
-    this._store.dispatch(GetComicsAction());
-  }
+	ngOnInit(): void {
+		this._store.dispatch(GetComicsAction());
+	}
 }
